3f50ea409b7ef71c3ee780431f52806bea12a08e
M.J.M Sakeen <jaleel.20211199@iit.ac.lk>
2023-08-29 22:15:53 +0530

    Adding dragging feature to the pieces

--- src/game.py
+++ src/game.py
@@ -2,11 +2,13 @@ import pygame
 
 from const import *
 from board import Board
+from dragger import Dragger
 
 
 class Game:
     def __init__(self):
         self.board = Board()
+        self.dragger = Dragger()
 
     def show_bg(self, surface):
         for row in range(ROWS):
@@ -29,4 +31,3 @@ class Game:
                     img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
                     piece.texture_rect = img.get_rect(center=img_center)
                     surface.blit(img, piece.texture_rect)
-
